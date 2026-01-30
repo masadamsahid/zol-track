15b1fd1fc2850b8364adc297cfb3bdcc1a1b8536
masadamsahid <masadamsahid@gmail.com>
2026-01-30 11:59:58 +0700

    add elysia-http-exception dependency and update CORS methods

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -3,6 +3,8 @@ import openapi from "@elysiajs/openapi";
 import { auth } from "@zol-track/auth";
 import { env } from "@zol-track/env/server";
 import { Elysia } from "elysia";
+import { httpExceptionPlugin } from "elysia-http-exception";
+import companiesRoutes from "./modules/companies";
 
 const app = new Elysia();
 
@@ -10,12 +12,13 @@ app
   .use(
     cors({
       origin: env.CORS_ORIGIN,
-      methods: ["GET", "POST", "OPTIONS"],
+      methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
       allowedHeaders: ["Content-Type", "Authorization"],
       credentials: true,
     }),
   )
   .use(openapi())
+  .use(httpExceptionPlugin())
   .all("/api/auth/*", async (context) => {
     const { request, status } = context;
     if (["POST", "GET"].includes(request.method)) {
