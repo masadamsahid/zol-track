339b9e96cdce0ff036d63ed4903ddecf4c6ba202
masadamsahid <masadamsahid@gmail.com>
2026-01-30 15:53:19 +0700

    feat: enhance error handling in httpExceptionPlugin with specific responses for validation errors

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -3,7 +3,7 @@ import openapi from "@elysiajs/openapi";
 import { auth } from "@zol-track/auth";
 import { env } from "@zol-track/env/server";
 import { Elysia } from "elysia";
-import { httpExceptionPlugin } from "elysia-http-exception";
+import { HttpException, httpExceptionPlugin } from "elysia-http-exception";
 import companiesRoutes from "./modules/companies";
 import { betterAuthMacro } from "./macros/auth.macros";
 
@@ -12,7 +12,7 @@ const app = new Elysia();
 app
   .use(
     cors({
-      origin:env.CORS_ORIGIN,
+      origin: env.CORS_ORIGIN,
       methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
       allowedHeaders: ["Content-Type", "Authorization"],
       credentials: true,
@@ -33,6 +33,35 @@ app
     }
   }))
   .use(httpExceptionPlugin())
+  .onError(({ code, error, set, status }) => {
+    // if (typeof code === 'number') {
+    //   if (code >= 400 && code < 600) {
+    //     set.status = code;
+    //     return { message: error.code };
+    //   }
+    //   return status(code, { message: 'Internal Server Error' });
+    // }
+    // switch (code) {
+    //   case 'VALIDATION':
+    //     return status(400, { message: error.message, errors: error.all });
+    //   case 'NOT_FOUND':
+    //     return status(404, { message: 'Resource Not Found' });     
+    //   case 'INTERNAL_SERVER_ERROR':
+    //   default:
+    //     return status(500, { message: 'Internal Server Error' });
+    // }
+    
+    if (error instanceof HttpException) {
+      set.status = error.statusCode;
+      if (code === 'VALIDATION') {
+        return { message: error.message, errors: error.all };
+      }
+      return { message: error.message };
+    }
+    
+    console.log(error);
+    return status(500, { message: 'Internal Server Error' });
+  })
   .all("/api/auth/*", async (context) => {
     const { request, status } = context;
     if (["POST", "GET"].includes(request.method)) {
